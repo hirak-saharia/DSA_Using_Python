5d1491e5a1d128f6a59bbc250f1cc6a9cbba87ff
hirak-saharia <hiraksaharia4@gmail.com>
2023-05-06 13:09:02 +0530

    added Stack-Constructor.py

---
+++ Stack_and_Queue/01-Stack-Constructor.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        
+        # Create a new instance of the Node class using the provided value.
+        new_node = Node(value)
+
+        # Set the top and bottom attribute of the Stack class to point to the new node.
+        self.top = new_node
+        self.bottom = new_node
+
+        # Initialize a height attribute for the Stack class, which represents the current number of nodes in the stack, and set it to 1
+        self.length = 1
+
+    def print_list(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+my_stack = Stack(4)
+
+my_stack.print_list()

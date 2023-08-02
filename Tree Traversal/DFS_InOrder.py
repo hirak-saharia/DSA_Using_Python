b5af796d006c43d03668328ed0228085b9d48660
hirak-saharia <hiraksaharia4@gmail.com>
2023-08-02 11:26:42 +0530

    added DFS_InOrder.py file

---
+++ Tree Traversal/DFS_InOrder.py
@@ -0,0 +1,133 @@
+# Write a function called dfs_in_order that performs a Depth-First Search(DFS) traversal on a binary tree using the In-Order approach
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while(True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while(temp):
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+    
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+    
+
+    def dfs_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
+    
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+    
+    def dfs_in_order(self):
+        results = [] # create an empty list to store the values of visited nodes
+
+        def traverse(current_node):
+            # if the current node has a left child, recursively traverse it
+            if current_node.left is not None:
+                traverse(current_node.left)
+
+            # append the value of the current node to the results list
+            results.append(current_node.value)
+
+            # if the current has a right child, recursively traverse it
+            if current_node.right is not None:
+                traverse(current_node.right)
+        
+        # start the in-order traversal from the root of the tree
+        traverse(self.root)
+
+        # return the list of visited node values
+        return results
+    
+
+
+
+
+my_tree = BinarySearchTree()
+
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.dfs_in_order())
